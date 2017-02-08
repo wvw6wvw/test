3452765ec0875326864d749582da185ebb0d72e0
zhang0808 <457074420@qq.com>
2017-02-08 15:15:17 +0800

    01 b.php

--- a.php
+++ a.php
@@ -1,4 +1,5 @@
-<?php
+﻿<?php
 //FTP地址：ftp://ftp1.linuxidc.com
 //用户名：ftp1.linuxidc.com
 //密码：www.linuxidc.com
+echo "abc";
---
+++ b.php
@@ -0,0 +1,26 @@
+<script src="templates/default/js/jquery-1.10.2.js"></script>
+<p>
+    <i onclick='chang_num(this);'>12</i>
+    
+</p>
+<script>
+    function chang_num(t){
+         $(t).parent().html("<input type='text' value='"+$(t).html()+"' name='num'>");
+        $('input[name="num"]').focus();
+         $('input[name="num"]').bind('blur',function (){
+             var v=$(this).val();
+             $(this).parent().html("<i onclick='chang_num(this);'>"+v+"</i>");
+         });
+        
+    }
+    //$("i").click(function (){
+       
+    //});
+    
+    // 
+    // 
+ // $(document).on('click',$("i"),function (){
+        //$('i').parent().html("<input type='text' value='2' name='num'>");
+        //$('input[name="num"]').focus();
+   // })
+</script>
